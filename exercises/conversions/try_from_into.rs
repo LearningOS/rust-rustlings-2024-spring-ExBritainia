da8d64f729fc5e2b7da11e267c6cf50de38b5b74
ExBritainia <150825904+ExBritainia@users.noreply.github.com>
2024-04-22 15:41:35 +0000

    update

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,7 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
@@ -46,7 +45,7 @@ impl TryFrom<(i16, i16, i16)> for Color {
         let b1:bool = b>=0 && b<=255;
         let c1:bool = c>=0 && c<=255;
         match (a1,b1,c1){
-            (true,true,true) => return Ok(Color{red:a as u8,blue:b as u8,green:c as u8}),
+            (true,true,true) => return Ok(Color{red:a as u8,green:b as u8,blue:c as u8}),
             (_,_,_) => return Err(IntoColorError::IntConversion),
         };
     }
@@ -56,6 +55,19 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if arr.len() != 3{
+            return Err(IntoColorError::BadLen)
+        }
+        let a = arr[0];
+        let b = arr[1];
+        let c = arr[2];
+        let a1:bool = a>=0 && a<=255;
+        let b1:bool = b>=0 && b<=255;
+        let c1:bool = c>=0 && c<=255;
+        match (a1,b1,c1){
+            (true,true,true) => return Ok(Color{red:a as u8,green:b as u8,blue:c as u8}),
+            (_,_,_) => return Err(IntoColorError::IntConversion),
+        };
     }
 }
 
@@ -63,6 +75,19 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3{
+            return Err(IntoColorError::BadLen)
+        }
+        let a = slice[0];
+        let b = slice[1];
+        let c = slice[2];
+        let a1:bool = a>=0 && a<=255;
+        let b1:bool = b>=0 && b<=255;
+        let c1:bool = c>=0 && c<=255;
+        match (a1,b1,c1){
+            (true,true,true) => return Ok(Color{red:a as u8,green:b as u8,blue:c as u8}),
+            (_,_,_) => return Err(IntoColorError::IntConversion),
+        };
     }
 }
 
